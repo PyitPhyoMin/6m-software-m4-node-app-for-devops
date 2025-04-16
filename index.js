8c299319dc8bb2d5a8887319a367e7ca3973631f
PyitPhyoMin <pyitphyo.min@gmail.com>
2025-04-17 03:02:50 +0800

    made changes for formatting

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 //const print = require("./controller");
-const controller = require('./controller');
+const controller = require("./controller");
 
-app.get('/', controller.print);
-app.get('/home', controller.home);
-app.post('/user', controller.user);
+app.get("/", controller.print);
+app.get("/home", controller.home);
+app.post("/user", controller.user);
 
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Listening to port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Listening to port ${process.env.PORT}`);
+});

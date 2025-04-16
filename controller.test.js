9c4cc6e17dcbcdcd5d5b934da777e44c5469a866
PyitPhyoMin <pyitphyo.min@gmail.com>
2025-04-16 20:32:46 +0800

    made changes for controller test

--- controller.test.js
+++ controller.test.js
@@ -1,4 +1,4 @@
-const targetFn = require("./controller");
+/* const targetFn = require("./controller");
 
 describe("the print function", ()=>{
 
@@ -15,5 +15,24 @@ describe("the print function", ()=>{
 
         targetFn(mockReq, mockRes);
         expect(mockRes.send).toHaveBeenCalledWith("Hello world!!"); // If you change this value, the test will fail.
+    })
+}) */
+
+const controller = require("./controller");
+
+describe("the print function", ()=>{
+    beforeEach(()=>{
+        jest.clearAllMocks();
+    })
+
+    it("should respond with 'Hello world!'", ()=>{
+        const mockReq = {};
+        const mockRes = {
+            send: jest.fn()
+        }
+
+        // Call the specific print function from the controller object
+        controller.print(mockReq, mockRes);
+        expect(mockRes.send).toHaveBeenCalledWith("Hello world!!");
     })
 })

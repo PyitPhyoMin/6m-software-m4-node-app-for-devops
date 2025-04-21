3374837769083425434546f954d0a22271a46202
PyitPhyoMin <pyitphyo.min@gmail.com>
2025-04-22 05:02:02 +0800

    update for controller

--- controller.js
+++ controller.js
@@ -9,10 +9,13 @@ function home(req, res){
     res.send("This is the home page");
 }
 
+const xss = require('xss');
 //POST /user: gets a user object (name, email, password) and prints the name and email
 function user(req, res){
     const user = req.body;
-    res.send(`Name: ${user.name}, Email: ${user.email}`);
+    const checkedName = xss(user.name || '');
+    const checkedEmail = xss(user.email || '');
+    res.send(`Name: ${checkedName}, Email: ${checkedEmail}`);
 }
 
 /* module.exports = print;
